e67cdd1632d9aa97476819aa064de7fbd39515fa
Unknown <8700106+devildant@users.noreply.github.com>
2020-05-25 20:25:17 +0200

    Z2 test

--- TH3DUF_R2/pins_RAMPS_CR10S.h
+++ TH3DUF_R2/pins_RAMPS_CR10S.h
@@ -107,10 +107,10 @@
 #define E0_ENABLE_PIN      24
 #define E0_CS_PIN          42
 
-#define E1_STEP_PIN        36
-#define E1_DIR_PIN         34
-#define E1_ENABLE_PIN      30
-#define E1_CS_PIN          44
+#define Z2_STEP_PIN        36
+#define Z2_DIR_PIN         34
+#define Z2_ENABLE_PIN      30
+#define Z2_CS_PIN          44
 
 
 //
